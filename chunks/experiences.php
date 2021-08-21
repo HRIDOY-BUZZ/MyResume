0fa266aae1c878c3a0d57c08189b016be62bb452
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2021-08-21 10:53:40 +0600

    qualification and experience updated

--- chunks/experiences.php
+++ chunks/experiences.php
@@ -8,25 +8,36 @@
                                 <div class="mh-education-deatils">
                                     <!-- Education Institutes-->
                                     <div class="mh-education-item dark-bg wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.3s">
-                                        <h4>B.Sc. in Computer Science and Engineering From <br>
+                                        <h4>Bachelor of Science (BSc) <i>From</i> <br>
                                         <a href="https://ewubd.edu/">East West University</a></h4>
+                                        <div class="mh-eduyear">Department of Computer Science and Engineering</div>
                                         <div class="mh-eduyear">2016-2020</div>
-                                        <p>A/2, Jahurul Islam Avenue, Jahurul Islam City, Aftabnagar, Dhaka-1212, Bangladesh </p>
+                                        <p><b>Campus:</b> A/2, Jahurul Islam Avenue, Jahurul Islam City, Aftabnagar,<br>Dhaka-1212, Bangladesh </p>
                                     </div>                                
                                     <!-- Education Institutes-->
                                     <div class="mh-education-item dark-bg wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.5s">
-                                        <h4>Higher Secondary School Certificate From <br>
+                                        <h4>Higher Secondary School Certificate (HSC) <i>From</i> <br>
                                         <a href="http://gsctd.edu.bd/en/">Govt. Science College (Dhaka Board)</a></h4>
+                                        <div class="mh-eduyear">Group: Science</div>
                                         <div class="mh-eduyear">2013-2015</div>
-                                        <p>34/B Farmgate - Tejturi Bazar Rd, Tejgaon, Dhaka 1215 </p>
+                                        <p><b>Campus:</b> 34/B Farmgate - Tejturi Bazar Rd, Tejgaon,<br>Dhaka-1215, Bangladesh </p>
                                     </div>                                
                                     <!-- Education Institutes-->
                                     <div class="mh-education-item dark-bg wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.6s">
-                                        <h4>Secondary School Certificate From <br>
+                                        <h4>Secondary School Certificate (SSC) <i>From</i> <br>
                                         <a href="https://www.motijheelmodelschoolandcollege.edu.bd/">Motijheel Model School & College (Dhaka Board)</a></h4>
+                                        <div class="mh-eduyear">Group: Science</div>
                                         <div class="mh-eduyear">2011-2013</div>
-                                        <p>Campus 1: Kamalapur Road, Motijheel, Dhaka-1000 <br>
-                                            Campus 2: Atish Dipankar Road, Bashabo, Shabujbag, Dhaka-1214 </p>
+                                        <p><b>Campus 1:</b> Kamalapur Road, Motijheel, Dhaka-1000, Bangladesh. <br>
+                                        <b>Campus 2:</b> Atish Dipankar Road, Bashabo, Shabujbag, Dhaka-1214, Bangladesh. </p>
+                                    </div>
+                                    <!-- Education Institutes-->
+                                    <div class="mh-education-item dark-bg wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.6s">
+                                        <h4>Junior School Certificate (JSC) <i>From</i> <br>
+                                        <a href="https://www.motijheelmodelschoolandcollege.edu.bd/">Motijheel Model School & College (Dhaka Board)</a></h4>
+                                        <div class="mh-eduyear">2010</div>
+                                        <p><b>Campus 1:</b> Kamalapur Road, Motijheel, Dhaka-1000, Bangladesh. <br>
+                                        <b>Campus 2:</b> Atish Dipankar Road, Bashabo, Shabujbag, Dhaka-1214, Bangladesh. </p>
                                     </div>
                                 </div>
                             </div>
@@ -37,9 +48,31 @@
                                 <div class="mh-experience-deatils">
                                     <!-- Workplace-->
                                     <div class="mh-work-item dark-bg wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s">
+                                        <h4>Web Developer @ <a href="http://xitsolution.com/">XiT Solutions BD</a></h4>
+                                        <div class="mh-eduyear">August 2021 - Present</div>
+                                        <span>Responsibilities :</span>
+                                        <ul class="work-responsibility">
+                                            <li><i class="fa fa-circle"></i>Web Back-end and Front-end Development</li>
+                                            <li><i class="fa fa-circle"></i>Web Back-end and Front-end Maintenance</li>
+                                            <li><i class="fa fa-circle"></i>Creating and Fixing Custom Scripts</li>
+                                        </ul>
+                                    </div>     
+                                    <!-- Workplace-->
+                                    <div class="mh-work-item dark-bg wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s">
+                                        <h4>Part-time Back-end Developer @ <a href="https://bmssystems.org/">BMS Systems</a></h4>
+                                        <div class="mh-eduyear">June 2021 - Present</div>
+                                        <span>Responsibilities :</span>
+                                        <ul class="work-responsibility">
+                                            <li><i class="fa fa-circle"></i>Web Back-end Development</li>
+                                            <li><i class="fa fa-circle"></i>App Back-end Development</li>
+                                            <li><i class="fa fa-circle"></i>Generating API</li>
+                                        </ul>
+                                    </div>      
+                                    <!-- Workplace-->
+                                    <div class="mh-work-item dark-bg wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s">
                                         <h4>Web Developer @ <a href="http://oxdoraitech.com/">Oxdora I Tech</a></h4>
                                         <div class="mh-eduyear">January 2021 - Present</div>
-                                        <span>Responsibility :</span>
+                                        <span>Responsibilities :</span>
                                         <ul class="work-responsibility">
                                             <li><i class="fa fa-circle"></i>Web Front-end Development</li>
                                             <li><i class="fa fa-circle"></i>Web Back-end Development</li>
@@ -50,10 +83,9 @@
                                     <div class="mh-work-item dark-bg wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.6s">
                                         <h4>Campus Ambassador @ <a href="https://creativeitinstitute.com/">Creative It Institute</a></h4>
                                         <div class="mh-eduyear">February 2020 - January 2021</div>
-                                        <span>Responsibility :</span>
+                                        <span>Responsibilities :</span>
                                         <ul class="work-responsibility">
-                                            <li><i class="fa fa-circle"></i>Let everyone know about the importance of It courses</li>
-                                            <li><i class="fa fa-circle"></i>Let everyone know about the benefits of Freelancing</li>
+                                            <li><i class="fa fa-circle"></i>Let everyone know about the importance and benefits of IT courses</li>
                                             <li><i class="fa fa-circle"></i>Let everyone know about the Latest course offers</li>
                                             <li><i class="fa fa-circle"></i>Attending Free sessions and Demo classes</li>
                                         </ul>
